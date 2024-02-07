e92176f7067eea04c41d578fb32c3d20e7d96007
TimRissel <76496934+TimRissel@users.noreply.github.com>
2024-02-07 12:55:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 let noCount = 0;
-let noButton = document.getElementById("noBtn");
-let responseIndex = 0;
 
 function changeResponse() {
-    if (noCount < 5 && noCount < funnyResponses.length) {
-        noButton.textContent = funnyResponses[responseIndex];
-        responseIndex = (responseIndex + 1) % funnyResponses.length;
-        noCount++;
-    } else if (noCount === 5) {
-        document.body.style.backgroundImage = "url('sad_teddy.gif')"; // GIF of sad teddy
-        document.getElementById("response").innerHTML = "Uh-oh, you've made the teddy sad!";
-        document.querySelector('.container').style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
-        noButton.style.display = "none"; // Hide the "No" button after the teddy appears
-        document.getElementById("response").style.display = "none"; // Hide the response text
+    let noButton = document.getElementById("noBtn");
+    if (noCount < 5) {
+        switch (noCount) {
+            case 0:
+                noButton.textContent = "Are you sure?";
+                break;
+            case 1:
+                noButton.textContent = "Think about it!";
+                break;
+            case 2:
+                noButton.textContent = "Maybe reconsider?";
+                break;
+            case 3:
+                noButton.textContent = "Last chance!";
+                break;
+            case 4:
+                noButton.textContent = "Are you really sure?";
+                break;
+            default:
+                break;
+        }
         noCount++;
     } else {
-        document.getElementById("response").innerHTML = "Are you sure?";
+        document.body.style.backgroundImage = "url('sad_teddy.jpg')"; // Change background to sad_teddy
+        noButton.style.display = "none"; // Hide the "No" button after the teddy appears
     }
 }
 
-function showHearts() {
-    document.body.style.backgroundImage = "url('love_teddy.gif')"; // GIF of love teddy
-    document.getElementById("response").innerHTML = "<h1>19:00 Uhr Gio Ristorante</h1><h1>I love you</h1>";
-    document.querySelector('.container').style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+function showInvitation() {
+    document.body.style.backgroundImage = "url('love_teddy.jpg')"; // Change background to love_teddy
+    document.querySelector('.container').innerHTML = "<h1>20:00 Uhr Gio Ristorante</h1>";
+    document.getElementById("response").innerHTML = "<h1>I love you</h1>";
 }
-
-const funnyResponses = [
-    "Nope, try again!",
-    "Still nope!",
-    "Are you kidding me?",
-    "Not happening!",
-    "You're persistent!",
-    "Last chance!",
-    "Seriously?",
-    "Really?",
-    "Alright, fine!",
-    "Oh, you finally gave in!",
-    // Add more funny responses here as needed
-];
